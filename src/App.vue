a154dc1d78ca7ee602fd1732cd71a9ee2884225a
fadhlu <fadhlurahman.i@gmail.com>
2020-06-19 09:54:07 +0700

    add transition

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <Header></Header>
     <div class="row mt-2">
       <div class="col-12">
-        <router-view></router-view>
+        <transition name="slide" mode="out-in">
+          <router-view></router-view>
+        </transition>
       </div>
     </div>
   </div>
@@ -18,6 +20,38 @@ export default {
   },
   created() {
     this.$store.dispatch('initStocks');
-  }
+  },
 };
 </script>
+
+<style>
+.slide-enter-active {
+  animation: slide-in 250ms forwards ease-out;
+}
+
+.slide-leave-active {
+  animation: slide-out 250ms forwards ease-out;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+}
+</style>
